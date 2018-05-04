50fbddf05d8351e8561bc12244811532acafa2fd
LemonIT.CN <liuri@lemonit.cn>
2018-05-05 07:07:05 +0800

    控制按钮区域添加共用的对话框

--- src/components/main/content/ControlButtonsModule.vue
+++ src/components/main/content/ControlButtonsModule.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">
+  <div class="control-buttons">
     <el-button type="success" size="small" icon="el-icon-caret-right">{{$t(lang + '.run')}}</el-button>
     <el-button type="success" size="small" icon="el-icon-upload" plain>{{$t(lang + '.upload_to') + ' ' + $t('system.cloud_name')}}</el-button>
     <el-button type="primary" size="small" icon="el-icon-star-on" plain>{{$t(lang + '.upload_to') + ' ' + $t('common.private_cloud')}}</el-button>
@@ -11,7 +11,7 @@
       :title="$t(lang + '.' + data_add_dialog_label.title)"
       :visible.sync="data_add_dialog_visible"
       width="50%">
-      <el-form ref="form" :model="data_add_dialog_form" >
+      <el-form :model="data_add_dialog_form" >
         <el-form-item :label="$t('common.please_input') + ' ' + $t('common.' + data_add_dialog_label.key) + ' : '">
           <el-input v-model="data_add_dialog_form.key"></el-input>
         </el-form-item>
@@ -20,7 +20,7 @@
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">{{$t('common.cancel')}}</el-button>
+        <el-button @click="data_add_dialog_visible = false">{{$t('common.cancel')}}</el-button>
         <el-button type="primary" @click="this[data_add_dialog_handler]">{{$t('common.create')}}</el-button>
       </span>
     </el-dialog>
@@ -49,15 +49,37 @@ export default {
       data_add_dialog_visible: false,
       // 对话框中的标签文字
       data_add_dialog_label: {
-        key: '',
-        name: '',
-        title: ''
+        key: 'instruction_set_key',
+        name: 'instruction_set_name',
+        title: 'add_instruction_set'
       },
       data_add_dialog_handler: 'handleAddInstructionSet',
       data_add_dialog_form: {
         key: '',
         name: ''
+      },
+      data_add_dialog_rules: {
+        key: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 2, max: 32, message: '长度在 2 到 32 个字符', trigger: 'blur' }
+        ],
+        name: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  watch: {
+    data_add_dialog_visible: {
+      handler (val, oldVal) {
+        if (val) {
+          // 打开对话框时候清空data_add_dialog_form对象
+          this.data_add_dialog_form.key = ''
+          this.data_add_dialog_form.name = ''
+        }
+      },
+      deep: true
     }
   }
 }
