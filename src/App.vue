872976a4835e618e339ec0d9efe7786476dcf5a6
LemonIT.CN <liuri@lemonit.cn>
2018-07-08 14:20:59 +0800

    将LRC模块抽出为单独的组件

--- src/App.vue
+++ src/App.vue
@@ -2,56 +2,19 @@
   <div id="app" v-loading.lock="$store.getters[this.$NS.GLOBAL_LOADING.GET_IS_SHOW]"
        :element-loading-text="$store.getters[this.$NS.GLOBAL_LOADING.GET_TITLE]">
     <router-view/>
-    <el-dialog
-      :title="$t('system.app_name')"
-      :visible.sync="$store.getters[$NS.CONNECTOR.GET_IS_SHOW_PANEL]"
-      width="30%">
-      <span>{{$t('connector.connector_tip_pre') + $t('system.app_name') +  $t('connector.connector_tip_end')}}</span>
-      <el-form>
-        <el-form-item :label="$t('connector.server_address')">
-          <el-input v-model="connector.address"></el-input>
-        </el-form-item>
-        <el-form-item label="LRCT">
-          <el-input v-model="connector.lrct"></el-input>
-        </el-form-item>
-        <el-form-item label="LRCK">
-          <el-input v-model="connector.lrck" type="textarea" :rows="4"></el-input>
-        </el-form-item>
-      </el-form>
-      <span slot="footer" class="dialog-footer">
-        <el-button type="success" @click="connect">{{$t('common.connect')}}</el-button>
-      </span>
-    </el-dialog>
+    <connector-request-panel/>
   </div>
 </template>
 
 <script>
+import ConnectorRequestPanel from '@/components/connector/request/ConnectorRequestPanelModule'
 export default {
+  components: {ConnectorRequestPanel},
   mounted () {
     // eslint-disable-next-line no-undef
     _lr.global = this.global
-    this.connector.address = localStorage.getconnector_address
-    this.connector.lrct = localStorage.getconnector_lrct
-    this.connector.lrck = localStorage.getconnector_lrck
-    console.log(this.$NS.CONNECTOR.GET_IS_SHOW_PANEL)
   },
-  name: 'App',
-  methods: {
-    connect () {
-      this.$store.dispatch(this.$NS.CONNECTOR.ACT_CONN_SUCCESS, {
-        state: true
-      })
-    }
-  },
-  data () {
-    return {
-      connector: {
-        address: '',
-        lrct: '',
-        lrck: ''
-      }
-    }
-  }
+  name: 'App'
 }
 </script>
 
---
+++ src/components/connector/request/ConnectorRequestPanelModule.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="connector-request-module">
+    <el-dialog
+      :title="$t('system.app_name')"
+      :visible.sync="$store.getters[$NS.CONNECTOR.GET_IS_SHOW_PANEL]"
+      width="30%">
+      <span>{{$t('connector.connector_tip_pre') + $t('system.app_name') +  $t('connector.connector_tip_end')}}</span>
+      <el-form>
+        <el-form-item :label="$t('connector.server_address')">
+          <el-input v-model="connector.address"></el-input>
+        </el-form-item>
+        <el-form-item label="LRCT">
+          <el-input v-model="connector.lrct"></el-input>
+        </el-form-item>
+        <el-form-item label="LRCK">
+          <el-input v-model="connector.lrck" type="textarea" :rows="4"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="success" @click="connect">{{$t('common.connect')}}</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConnectorRequestPanelModule',
+  mounted () {
+    this.connector.address = localStorage.getconnector_address
+    this.connector.lrct = localStorage.getconnector_lrct
+    this.connector.lrck = localStorage.getconnector_lrck
+    console.log(this.$NS.CONNECTOR.GET_IS_SHOW_PANEL)
+  },
+  methods: {
+    connect () {
+      this.$store.dispatch(this.$NS.CONNECTOR.ACT_CONN_SUCCESS, {
+        state: true
+      })
+    }
+  },
+  data () {
+    return {
+      connector: {
+        address: '',
+        lrct: '',
+        lrck: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .connector-request-module {
+  }
+
+</style>
